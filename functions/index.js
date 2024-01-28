ce7862eb31cad8658aa82829feba1f5fb7d96553
Znyck <corporate.znyck@gmail.com>
2024-01-28 23:10:51 +0530

    chages in itendetionn

--- functions/index.js
+++ functions/index.js
@@ -1,28 +1,33 @@
 const functions = require('firebase-functions');
 const { PredictionServiceClient } = require('@google-cloud/aiplatform');
 const admin = require('firebase-admin');
+
 admin.initializeApp();
 
-const PROJECT_ID = 'oval-bot-409716'; // Replace with your Google Cloud project ID
-const MODEL = 'textembedding-gecko@003'; // Replace with your AI Platform endpoint ID
-
+// constans
+const PROJECT_ID = 'oval-bot-409716'; 
+const MODEL = 'textembedding-gecko@003'; 
+const modelEndpoint = `projects/${PROJECT_ID}/locations/us-central1/publishers/google/models/${MODEL}`;
+const API_ENDPOINT = `https://us-central1-aiplatform.googleapis.com/v1/${modelEndpoint}:predict`;
+const TASK_TYPE = 'RETRIEVAL_DOCUMENT'
 exports.predictFunction = functions.https.onRequest(async (req, res) => {
   try {
-    
+    // Validate that the request method
+    if (req.method !== 'POST') {
+      return res.status(405).send('Method Not Allowed');
+    }
     // Validate that the request has a body and content property
     if (!req.body || !req.body.content || !req.body.title || !req.body.secret_code || req.body.secret_code  !== functions.config().config.secret_code) {
       res.status(400).json({ error: 'Bad Request', details: 'Request body error.'});
       return;
     }
 
-   
-    const modelEndpoint = `projects/${PROJECT_ID}/locations/us-central1/publishers/google/models/${MODEL}`;
-    // const [accessTokenResponse] = await client.auth.getAccessToken();
-
-    const API_ENDPOINT = `https://us-central1-aiplatform.googleapis.com/v1/${modelEndpoint}:predict`;
-
     const data = {
-      instances: [{ task_type: 'RETRIEVAL_DOCUMENT', title: req.body.title, content: req.body.content }],
+      instances: [{ 
+        task_type: TASK_TYPE, 
+        title: req.body.title, 
+        content: req.body.content 
+      }],
     };
 
     const response = await fetch(API_ENDPOINT, {
