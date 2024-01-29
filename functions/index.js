3aaa26399846c6392959ac196ac5fdd3266a3d0d
Znyck <corporate.znyck@gmail.com>
2024-01-29 20:09:00 +0530

    MVC redy setup

--- functions/index.js
+++ functions/index.js
@@ -1,50 +1,79 @@
 const functions = require('firebase-functions');
-const { PredictionServiceClient } = require('@google-cloud/aiplatform');
 const admin = require('firebase-admin');
+const aiplatform = require('@google-cloud/aiplatform');
+
+const {PredictionServiceClient} = aiplatform.v1;
+
+const {helpers} = aiplatform;
 
 admin.initializeApp();
 
-// constans
-const PROJECT_ID = 'oval-bot-409716'; 
+// Constants
+const PROJECT_ID = 'oval-bot-409716';
+const PUBLISHER = 'google';
 const MODEL = 'textembedding-gecko@003'; 
-const modelEndpoint = `projects/${PROJECT_ID}/locations/us-central1/publishers/google/models/${MODEL}`;
-const API_ENDPOINT = `https://us-central1-aiplatform.googleapis.com/v1/${modelEndpoint}:predict`;
-const TASK_TYPE = 'RETRIEVAL_DOCUMENT'
+const location = 'us-central1';
+const TASK_TYPE = 'RETRIEVAL_DOCUMENT';
+
+const clientOptions = {
+  apiEndpoint: 'us-central1-aiplatform.googleapis.com',
+};
+
+const predictionServiceClient = new PredictionServiceClient(clientOptions);
+
+async function callPredict(req,res) {
+  const endpoint = `projects/${PROJECT_ID}/locations/${location}/publishers/${PUBLISHER}/models/${MODEL}`;
+  const instance = { 
+      task_type: TASK_TYPE, 
+      title: req.body.title, 
+      content: req.body.content 
+   };
+  const instanceValue = helpers.toValue(instance);
+  const instances = [instanceValue];
+
+  const parameter = {
+    temperature: 0,
+    maxOutputTokens: 256, 
+    topP: 0, 
+    topK: 1 
+  };
+  const parameters = helpers.toValue(parameter);
+
+  const request = { endpoint, instances, parameters };
+
+  // Predict request
+  return await predictionServiceClient.predict(request).then((_res)=>{
+    return res.status(200).send({data : _res});
+  }).catch((err)=>{
+    return res.status(500).json({ error: 'Internal Server Error', details: err.message });
+  });
+}
+
 exports.predictFunction = functions.https.onRequest(async (req, res) => {
   try {
     // Validate that the request method
     if (req.method !== 'POST') {
       return res.status(405).send('Method Not Allowed');
     }
-    // Validate that the request has a body and content property
-    if (!req.body || !req.body.content || !req.body.title || !req.body.secret_code || req.body.secret_code  !== functions.config().config.secret_code) {
-      res.status(400).json({ error: 'Bad Request', details: 'Request body error.'});
-      return;
+
+    //Validate that the request has a body and content property
+    if (!req.body || !req.body.content || !req.body.title || !req.body.secret_code || req.body.secret_code !== functions.config().config.secret_code) {
+      return res.status(400).json({ error: 'Bad Request', details: 'Request body error.' });
     }
 
-    const data = {
-      instances: [{ 
-        task_type: TASK_TYPE, 
-        title: req.body.title, 
-        content: req.body.content 
-      }],
-    };
-
-    const response = await fetch(API_ENDPOINT, {
-      method: 'POST',
-      headers: {
-        'Authorization': `Bearer ${functions.config().config.token}`,
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data),
-    });
-
-    const responseData = await response.json();
-
-    return res.status(200).send(responseData);
+    //length validation
+    if (req.body.title.length >= 100) {
+      return res.status(400).json({ error: 'Bad Request', details: 'Request body title should not contain more than 100 characters' });
+    }
+    
+    if (req.body.content.length >= 2000) {
+      return res.status(400).json({ error: 'Bad Request', details: 'Request body content should not contain more than 2000 characters.' });
+    }
+    
+    // Call AI Platform Prediction
+    return await callPredict(req,res)
 
   } catch (error) {
-    console.error('Error making prediction:', error.response ? error.response.data : error.message);
     return res.status(500).json({ error: 'Internal Server Error', details: error.message });
   }
 });
